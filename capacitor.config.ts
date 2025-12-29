44a6aa5560d2b39194fdca777443ac44009409c9
Ayushi <ayushisingh637011@gmail.com>
2025-12-29 13:37:56 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ayushi.studyplanner',
+  appName: 'StudyPlanner',
+  webDir: '.'
+};
+
+export default config;
